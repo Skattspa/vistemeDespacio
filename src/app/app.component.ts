41d0cae0647394f1df6eb40739e2bd8816702b40
Sk4tt <j.madrigalvergel@gmail.com>
2019-11-26 20:20:15 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'vistemeDespacio';
+  title = 'vistemeDespacio - Los heroes de la fuerza de la voluntad';
+  arrayOfStrings = [
+    'one',
+    'two',
+    'three'
+  ];
+  arrayOfObjetcs = [
+    {
+      vegetal: 'patata',
+      salsa: 'ketchup'
+    },
+    {
+      vegetal: 'zanahoria',
+      salsa: 'mayonesa'
+    },
+    {
+      vegetal: 'lechuga',
+      salsa: 'aceite'
+    }
+  ]
 }
